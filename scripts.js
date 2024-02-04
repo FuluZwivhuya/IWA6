3fa9be5145ff13b5c5ca478c82f6da1dc2b57520
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-04 19:41:25 +0200

    challenge 1 done

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const primaryPhone = "O748105141";
+const secondaryPhone = "0219131568";
+
+// Only change below this line
+let primaryValid = typeof number == primaryPhone 
+primaryValid = parseInt(primaryPhone)
+
+let secondaryValid = typeof number == secondaryPhone
+secondaryValid = parseInt(secondaryPhone);
+
+console.log('Primary phone is valid numerical string:', primaryPhone == primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryPhone == secondaryValid);
+
+
+/*//const primaryValid = typeof number === primaryPhone;
+console.log(typeof primaryPhone);
+let primaryValidvalid = Number(secondaryPhone);
+console.log(typeof primaryvalid);
+//const secondaryValid = typeof number === secondaryPhone;
+
+let secondaryValid = Number(secondaryPhone);
+console.log(typeof secondaryValid);
+secondaryValid = typeof number === secondaryPhone;
+
+//console.log("Primary phone is valid numerical string:", primaryValid);
+console.log("Secondary phone is valid numerical string:", secondaryValid);*/
